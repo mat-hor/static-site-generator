e8d906b0464def587879d65f226e5507bc06f885
mat-hor <matt.hor.dev@gmail.com>
2025-05-24 14:48:14 +0200

    fun to get type of block

--- src/markdown_utils.py
+++ src/markdown_utils.py
@@ -1,4 +1,13 @@
 import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPG = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 def extract_markdown_images(text):
     matches = re.findall(r"\!\[(.*?)\]\((.*?)\)", text)
@@ -19,14 +28,75 @@ def markdown_to_blocks(markdown):
     return blocks
 
 
-markdown = """
-# This is a heading
+def block_to_block_type(block):
+    lines = block.split("\n")
+    hash_char = "#"
+    order_number = 1
+    for line in lines:
+        result = BlockType.PARAGRAPG
+        for _ in range(0, 6):
+            if re.findall(r"^" + hash_char + " ", line):
+                result = BlockType.HEADING
+            hash_char += "#"
+        if re.findall(r"```[\s\S]*?```", line):
+                result = BlockType.CODE
+        if re.findall(r">", line):
+                result = BlockType.QUOTE
+        if re.findall(r"- ", line):
+                result = BlockType.UNORDERED_LIST
+        if re.findall(str(order_number) + ". ", line):
+                result = BlockType.ORDERED_LIST
+                order_number += 1
+        
+    return result
+        
 
-This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
 
-- This is the first list item in a list block
-- This is a list item
-- This is another list item
+# md = """
+# This is **bolded** paragraph
+
+# This is another paragraph with _italic_ text and `code` here
+# This is the same paragraph on a new line
+
+# . This is a list
+# - with items
+# """
+md = """
+# Heading 1
+
+## Heading 2
+
+### Heading 3
+
+#### Heading 4
+
+##### Heading 5
+
+###### Heading 6
+
+This is a paragraph of text.
+
+This is a **bold** word.
+
+This is an _italic_ word.
+
+This is a paragraph with a [link](https://www.google.com).
+
+- Item 1
+- Item 2
+- Item 3
+
+1. Item 1
+2. Item 2
+3. Item 3
+
+```
+This is code
+```
 """
+result = []
+blocks = markdown_to_blocks(md)
+for block in blocks:
+    result.append(block_to_block_type(block).value)
 
-print(markdown_to_blocks(markdown))
+print(result)
--- src/test_markdown_utils.py
+++ src/test_markdown_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_utils import extract_markdown_images, extract_markdown_links, markdown_to_blocks
+from markdown_utils import extract_markdown_images, extract_markdown_links, markdown_to_blocks, block_to_block_type
 
 class TestMarkdownUtils(unittest.TestCase):
     def test_extract_markdown_images(self):
@@ -33,6 +33,49 @@ This is the same paragraph on a new line
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+    def test_block_to_block_type(self):
+        md = """
+# Heading 1
+
+## Heading 2
+
+### Heading 3
+
+#### Heading 4
+
+##### Heading 5
+
+###### Heading 6
+
+This is a paragraph of text.
+
+This is a **bold** word.
+
+This is an _italic_ word.
+
+This is a paragraph with a [link](https://www.google.com).
+
+- Item 1
+- Item 2
+- Item 3
+
+1. Item 1
+2. Item 2
+3. Item 3
+
+```
+This is code
+```
+"""
+        result = []
+        blocks = markdown_to_blocks(md)
+        for block in blocks:
+            result.append(block_to_block_type(block).value)
+        self.assertEqual(
+            result,
+            ['heading', 'heading', 'heading', 'heading', 'heading', 'heading', 'paragraph', 'paragraph', 'paragraph', 'paragraph', 'unordered_list', 'ordered_list', 'paragraph'],
         )
 
         
